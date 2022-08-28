695a69e4a4a9cdab563491c69a11aead9cc1f65f
Arif Ahmed <smarifahmed42@gmail.com>
2022-08-28 20:45:59 +0600

    Initial Commit

---
+++ CustomAlert-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomAlert-Swift
+//
+//  Created by SM Arif Ahmed on 28/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
